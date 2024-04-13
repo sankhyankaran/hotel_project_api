79ad7a732cd849f6531b666078e32c400cecc98f
sankhyankaran <karansrma2003@gmail.com>
2024-04-13 23:45:10 +0530

    middleware entry  time apply all routes

--- server.js
+++ server.js
@@ -3,10 +3,20 @@ const app = express();
 const db = require("./db");
 require("dotenv").config();
 
+const PORT = process.env.PORT || 2004;
+
 const bodyParser = require("body-parser");
 app.use(bodyParser.json());
 
-// Person routes
+const logRequest = (req, res, next) => {
+  console.log(
+    `${new Date().toLocaleString()} Request made to: ${req.originalUrl}`
+  );
+  next();
+};
+
+app.use(logRequest);
+//Person routes
 const personRoute = require("./routes/personRoute");
 app.use("/person", personRoute);
 
@@ -18,8 +28,6 @@ app.get("/", (req, res) => {
   res.send("Data is Connected to Our Hotel database");
   console.log("Data is connected to server");
 });
-
-const PORT = process.env.PORT || 2004;
 
 app.listen(PORT, () => {
   console.log("Server is running on port " + PORT);
